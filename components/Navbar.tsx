919bc47dbfe99ebf4ea3e0386c7bd4670278e2fd
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-22 22:13:36 +0530

    added: brand link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,17 +3,11 @@ import { MdDarkMode } from "react-icons/md";
 const Navbar = (): React.ReactElement => {
   return (
     <nav className="fixed left-0 right-0 top-0 flex h-16 items-center justify-between border-b border-gray-300 bg-light-foreground px-10 shadow-sm">
-      <div className="">
-        <Link
-          href="/"
-          className="text-2xl font-semibold"
-        >
-          NOTES
-        </Link>
-      </div>
-      <div className=""></div>
-      <ul className="flex items-center gap-5">
-        <li className="rounded-md p-2 shadow-sm hover:cursor-pointer">
+      <span className="text-2xl font-bold">
+        <Link href="/">Notation</Link>
+      </span>
+      <ul>
+        <li>
           <MdDarkMode size={25} />
         </li>
       </ul>
