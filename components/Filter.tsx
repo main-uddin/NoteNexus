4dd927d425354a85605aad7f26ec4dce7b510597
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-24 00:00:40 +0530

    added: filter initial state

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -1,3 +1,21 @@
+const filterInitialState = {
+  sort_by_time: [
+    { id: "qwe", name: "New To Old" },
+    { id: "etw", name: "Old To New" },
+  ],
+  priority: [
+    { id: "etweaw", name: "High" },
+    { id: "etcr4w", name: "Medium" },
+    ,
+    { id: "etf3tyw", name: "Low" },
+  ],
+  labels: [
+    { id: "jojw", name: "Home" },
+    { id: "jmdw", name: "Work" },
+    { id: "wmci", name: "Personal" },
+  ],
+};
+
 const Filter = (): React.ReactElement => {
   return (
     <div className="absolute top-16 right-0 z-10 h-fit w-56 rounded-md border border-gray-600 bg-light-foreground py-1 px-2 shadow-md">
@@ -9,8 +27,13 @@ const Filter = (): React.ReactElement => {
             name=""
             id=""
           >
-            <option>New To Old</option>
-            <option>Old To New</option>
+            {filterInitialState.sort_by_time.map(
+              (x) => (
+                <option key={x.id}>
+                  {x.name}
+                </option>
+              )
+            )}
           </select>
         </span>
         <span className="flex flex-col space-y-1">
@@ -22,41 +45,32 @@ const Filter = (): React.ReactElement => {
             name=""
             id=""
           >
-            <option>High</option>
-            <option>Medium</option>
-            <option>Low</option>
+            {filterInitialState.priority.map(
+              (x) => (
+                <option key={x?.id}>
+                  {x?.name}
+                </option>
+              )
+            )}
           </select>
         </span>
       </div>
       <span className="">Filter By Labels</span>
       <ul className="">
-        <li className="flex items-center gap-2">
-          <input
-            className="hover:cursor-pointer"
-            type="checkbox"
-            name=""
-            id=""
-          />{" "}
-          Home
-        </li>
-        <li className="flex items-center gap-2">
-          <input
-            className="hover:cursor-pointer"
-            type="checkbox"
-            name=""
-            id=""
-          />
-          Work
-        </li>
-        <li className="flex items-center gap-2">
-          <input
-            className="hover:cursor-pointer"
-            type="checkbox"
-            name=""
-            id=""
-          />
-          Personal
-        </li>
+        {filterInitialState.labels.map((x) => (
+          <li
+            key={x.id}
+            className="flex items-center gap-2"
+          >
+            <input
+              className="hover:cursor-pointer"
+              type="checkbox"
+              name=""
+              id=""
+            />{" "}
+            {x.name}
+          </li>
+        ))}
       </ul>
     </div>
   );
