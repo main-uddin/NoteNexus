ed063151f0abe9c66b2465685cda0100cf56ef88
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-25 10:00:19 +0530

    added: react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,22 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../components";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App({
   Component,
   pageProps,
 }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </QueryClientProvider>
   );
 }
