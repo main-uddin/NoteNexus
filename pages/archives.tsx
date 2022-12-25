20a2e32233ea32c59fb0a750571e39dda4cfd83c
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-12-25 20:31:47 +0530

    Revert "added: sidebar layout"
    This reverts commit 6dbb6a911c880f0b476fbe8481d66a1f8b1ed2be.

--- pages/archives.tsx
+++ pages/archives.tsx
@@ -1,7 +1,4 @@
-import {
-  NotesCard,
-  SidebarLayout,
-} from "../components";
+import { NotesCard } from "../components";
 import { useGetDoc } from "../hooks";
 import { Note } from "../types";
 
@@ -16,27 +13,25 @@ const ArchivesPage = (): React.ReactElement => {
     return <h1>loading...</h1>;
 
   return (
-    <SidebarLayout>
-      <div className="mx-auto mt-20 -translate-x-16 space-y-5 md:w-4/5 lg:w-1/2">
-        <div className="flex justify-center">
-          <h1 className="text-center text-xl font-semibold">
-            Archived Notes
-          </h1>
-        </div>
-        <div className="space-y-5">
-          {userData?.notes
-            .filter(
-              (x: Note) => x.archive && !x.trash
-            )
-            .map((x: Note) => (
-              <NotesCard
-                noteData={x}
-                userNotesData={userData.notes}
-              />
-            ))}
-        </div>
+    <div className="mx-auto mt-20 space-y-5 md:w-4/5 lg:w-2/5">
+      <div className="flex justify-center">
+        <h1 className="text-center text-xl font-semibold">
+          Archived Notes
+        </h1>
       </div>
-    </SidebarLayout>
+      <div className="space-y-5">
+        {userData?.notes
+          .filter(
+            (x: Note) => x.archive && !x.trash
+          )
+          .map((x: Note) => (
+            <NotesCard
+              noteData={x}
+              userNotesData={userData.notes}
+            />
+          ))}
+      </div>
+    </div>
   );
 };
 
--- pages/trash.tsx
+++ pages/trash.tsx
@@ -1,7 +1,4 @@
-import {
-  NotesCard,
-  SidebarLayout,
-} from "../components";
+import { NotesCard } from "../components";
 import { useGetDoc } from "../hooks";
 import { Note } from "../types";
 
@@ -15,28 +12,28 @@ const TrashPage = (): React.ReactElement => {
   if (isUserDataLoading)
     return <h1>loading...</h1>;
 
+  console.log(userData);
+
   return (
-    <SidebarLayout>
-      <div className="mx-auto mt-20 -translate-x-16 space-y-5 md:w-4/5 lg:w-2/5">
-        <div className="flex justify-center">
-          <h1 className="text-center text-xl font-semibold">
-            Trashed Notes
-          </h1>
-        </div>
-        <div className="space-y-5">
-          {userData?.notes
-            .filter(
-              (x: Note) => !x.archive && x.trash
-            )
-            .map((x: Note) => (
-              <NotesCard
-                noteData={x}
-                userNotesData={userData.notes}
-              />
-            ))}
-        </div>
+    <div className="mx-auto mt-20 space-y-5 md:w-4/5 lg:w-2/5">
+      <div className="flex justify-center">
+        <h1 className="text-center text-xl font-semibold">
+          Trashed Notes
+        </h1>
       </div>
-    </SidebarLayout>
+      <div className="space-y-5">
+        {userData?.notes
+          .filter(
+            (x: Note) => !x.archive && x.trash
+          )
+          .map((x: Note) => (
+            <NotesCard
+              noteData={x}
+              userNotesData={userData.notes}
+            />
+          ))}
+      </div>
+    </div>
   );
 };
 
