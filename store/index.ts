a498220fcab609eb3f8fda1d304f0eec8ab5777d
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-02-01 12:30:54 +0530

    refactor: removed user store

--- store/index.ts
+++ store/index.ts
@@ -2,12 +2,10 @@ import useAuthStore from "./useAuthStore";
 import useDarkTheme from "./useDarkTheme";
 import useFilterStore from "./useFilterStore";
 import useToggleNoteStore from "./useToggleNoteStore";
-import useUserStore from "./useUserStore";
 
 export {
   useAuthStore,
   useFilterStore,
-  useUserStore,
   useDarkTheme,
   useToggleNoteStore,
 };
