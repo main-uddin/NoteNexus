731738e8e0373445c99399982b7d133a0d59c434
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-02-05 18:24:11 +0530

    style: responsive breakpoints

--- components/SidebarLayout.tsx
+++ components/SidebarLayout.tsx
@@ -7,10 +7,12 @@ const SidebarLayout = ({
 }): React.ReactElement => {
   return (
     <div className="grid grid-cols-12 gap-5">
-      <div className="sticky col-span-2 min-h-screen bg-light-foreground">
+      <div className="sticky col-span-2 min-h-screen bg-light-foreground sm:hidden lg:block">
         <Sidebar />
       </div>
-      <div className="col-span-10">{children}</div>
+      <div className="sm:col-span-12 lg:col-span-10">
+        {children}
+      </div>
     </div>
   );
 };
--- pages/trash.tsx
+++ pages/trash.tsx
@@ -18,7 +18,7 @@ const TrashPage = (): React.ReactElement => {
         <title>Trashed Notes | Notation</title>
       </Head>
       <SidebarLayout>
-        <div className="mx-auto mt-20 space-y-5 md:w-4/5 lg:w-2/5">
+        <div className="mx-auto mt-20 space-y-5 pb-8 sm:w-11/12 md:w-4/5 lg:w-2/4">
           <div className="flex justify-center">
             <h1 className="text-center text-xl font-semibold">
               Trashed Notes
