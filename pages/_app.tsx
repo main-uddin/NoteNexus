fd5cc67c0e7d507ba7e597ff884a91e7cab0873c
Main-uddin-dhk <main@dhakai.com>
2024-05-25 01:42:41 +0600

    fix type error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,34 +3,22 @@
 // import { Layout } from "../components";
 // import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-// const queryClient = new QueryClient();
-
-// export default function App({ Component, pageProps }: AppProps) {
-//     return (
-//         <QueryClientProvider client={queryClient}>
-//             <Layout>
-//                 <Component {...pageProps} />
-//             </Layout>
-//         </QueryClientProvider>
-//     );
-// }
-
-import { AppProps } from "next/app";
+import { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // import { QueryClient, QueryClientProvider } from 'react-query';
-import Layout from "../components/Layout";
-import "../styles/globals.css"; // Make sure this path is correct
+import Layout from '../components/Layout';
+import '../styles/globals.css';
 
 const queryClient = new QueryClient();
 
-function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </QueryClientProvider>
-    );
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
